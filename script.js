4e1318d7f5c80c06a9438664af37dfbcd5212cfe
Pierre <pierre.laffaille@outlook.fr>
2024-12-04 16:35:30 +0100

    ajout de la fonction pour ajouter des nouvelles tâches dans le localStorage

--- script.js
+++ script.js
@@ -1,54 +1,73 @@
-let tasks = [
-  {
-    id: 1,
-    name: "lessive",
-    completed: false,
-  },
-  {
-    id: 2,
-    name: "vaiselle",
-    completed: true,
-  },
-];
+/* let tasks = [
+	{
+		id: 1,
+		name: "lessive",
+		completed: false,
+	},
+	{
+		id: 2,
+		name: "vaiselle",
+		completed: true,
+	},
+]; */
 
 const recupDonnees = () => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve();
-    }, 2000);
-  }).then(() => {
-    return tasks;
-  });
+	return new Promise((resolve) => {
+		setTimeout(() => {
+			resolve();
+		}, 2000);
+	}).then(() => {
+		return tasks;
+	});
 };
 recupDonnees();
 
 const affiche = (tableau) => {
-  tableau.forEach((element) => {
-    const nom = element.name;
-    const ligne = document.createElement("tr");
-    const celluleNom = document.createElement("td");
-    const celluleOk = document.createElement("td");
-    const celluleSupr = document.createElement("td");
+	tableau.forEach((element) => {
+		const nom = element.name;
+		const ligne = document.createElement("tr");
+		const celluleNom = document.createElement("td");
+		const celluleOk = document.createElement("td");
+		const celluleSupr = document.createElement("td");
 
-    celluleNom.textContent = nom;
+		celluleNom.textContent = nom;
 
-    const boutonOk = document.createElement("button");
-    if (element.completed) {
-      boutonOk.textContent = "terminé";
-    } else {
-      boutonOk.textContent = "pas fait";
-    }
-    celluleOk.appendChild(boutonOk);
+		const boutonOk = document.createElement("button");
+		if (element.completed) {
+			boutonOk.textContent = "terminé";
+		} else {
+			boutonOk.textContent = "pas fait";
+		}
+		celluleOk.appendChild(boutonOk);
 
-    const boutonSup = document.createElement("button");
-    boutonSup.textContent = "supprimé";
-    celluleSupr.appendChild(boutonSup);
+		const boutonSup = document.createElement("button");
+		boutonSup.textContent = "supprimé";
+		celluleSupr.appendChild(boutonSup);
 
-    ligne.appendChild(celluleNom, celluleOk, celluleSupr);
-    ligne.appendChild(celluleOk);
-    ligne.appendChild(celluleSupr);
+		ligne.appendChild(celluleNom, celluleOk, celluleSupr);
+		ligne.appendChild(celluleOk);
+		ligne.appendChild(celluleSupr);
 
-    document.getElementById("tasktable").appendChild(ligne);
-  });
+		document.getElementById("tasktable").appendChild(ligne);
+	});
 };
-affiche(tasks);
+
+let id = 0;
+let tasks = [];
+
+const addTask = (arr) => {
+	const taskInput = document.querySelector("input");
+	const task = {
+		id: `${id++}`,
+		name: `${taskInput.value}`,
+		completed: false,
+	};
+	arr.push(task);
+	localStorage.setItem("task-array", JSON.stringify(arr));
+};
+
+const form = document.querySelector("form");
+form.addEventListener("submit", (e) => {
+	e.preventDefault();
+	addTask(tasks);
+});
