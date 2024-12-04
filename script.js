2524aadea8ebffbba240538197cf406bc3c16564
Nicolasch64 <nidichapelle2000@yahoo.fr>
2024-12-04 15:04:17 +0100

    création de la fonction pour recupérer les données

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 let tasks = [
-	{
-		id: 1,
-		name: "lessive",
-		completed: false,
-	},
-	{
-		id: 2,
-		name: "vaiselle",
-		completed: false,
-	},
+  {
+    id: 1,
+    name: "lessive",
+    completed: false,
+  },
+  {
+    id: 2,
+    name: "vaiselle",
+    completed: false,
+  },
 ];
+
+const recupDonnees = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, 2000);
+  }).then(() => {
+    console.log(tasks);
+  });
+};
+recupDonnees();
